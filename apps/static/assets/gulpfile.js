26dce15a450265c4d05085fdc54bf4aeaeac8232
App Generator <51070104+app-generator@users.noreply.github.com>
2022-12-13 19:44:08 +0200

    Import Gulp Script from Original KIT

--- apps/static/assets/gulpfile.js
+++ apps/static/assets/gulpfile.js
@@ -6,54 +6,210 @@
 
 */
 
-var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync').create();
-var cleanCss = require('gulp-clean-css');
-var gulp = require('gulp');
-const npmDist = require('gulp-npm-dist');
-var sass = require('gulp-sass')(require('node-sass'));
-var wait = require('gulp-wait');
-var sourcemaps = require('gulp-sourcemaps');
-var rename = require("gulp-rename");
+var gulp = require("gulp"), // main
+  sass = require("gulp-sass")(require("sass")), // scss compiler
+  del = require('del'), // demove directory
+  sourcemaps = require("gulp-sourcemaps"), // scss sourcemaps
+  uglify = require("gulp-uglify"), // minify js files
+  sourcemaps = require("gulp-sourcemaps"), // scss sourcemaps
+  cssmin = require("gulp-cssmin"), // minify css files
+  merge = require("merge-stream"), // mearge two task
+  babel = require("gulp-babel"), // convert next generation JavaScript, today.
+  npmlodash = require("lodash"), // perfoming oops in gulp
+  smushit = require("gulp-smushit"), // image optimizer
+  autoprefixer = require("gulp-autoprefixer"), // css propertys autoprefixer
+  cssbeautify = require("gulp-cssbeautify"), // css cssbeautify
+  fileinclude = require("gulp-file-include"), // include html files
+  browsersync = require("browser-sync"), // browser reload
+  htmlmin = require("gulp-htmlmin"); // html minify
 
-// Define COMMON paths
+//  [ scss compiler ] start
+gulp.task("sass", function () {
+  // main style css
+  return gulp
+    .src("scss/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write())
+    // .pipe(cssbeautify())
+    .pipe(gulp.dest("css"));
+});
+//  [ scss compiler ] end
 
-const paths = {
-    src: {
-        base: './',
-        css: './css',
-        scss: './scss',
-        node_modules: './node_modules/',
-        vendor: './vendor'
+//  [ Copy assets ] start
+gulp.task("build", function () {
+  var required_libs = {
+    js: [
+      "node_modules/bootstrap/dist/js/bootstrap.min.js",
+      "node_modules/@popperjs/core/dist/umd/popper.min.js",
+      "node_modules/simplebar/dist/simplebar.min.js",
+      "node_modules/clipboard/dist/clipboard.min.js",
+      "node_modules/apexcharts/dist/apexcharts.min.js",
+    ],
+    css: [
+      "node_modules/bootstrap/dist/css/bootstrap.min.css",
+      "node_modules/animate.css/animate.min.css",
+    ],
+  };
+  npmlodash(required_libs).forEach(function (assets, type) {
+    if (type == "css") {
+      gulp.src(assets).pipe(gulp.dest("dist/assets/css/plugins"));
+    } else {
+      gulp.src(assets).pipe(gulp.dest("dist/assets/js/plugins"));
     }
-};
+  });
+  var cpyassets = gulp
+    .src(["src/assets/**/*.*", "!src/assets/scss/**/*.*"])
+    .pipe(gulp.dest("dist/assets"));
 
-// Compile SCSS
-gulp.task('scss', function() {
-    return gulp.src([paths.src.scss + '/material-kit.scss'])
-        .pipe(wait(500))
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            overrideBrowserslist: ['> 1%']
-        }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(paths.src.css))
-        .pipe(browserSync.stream());
+  return merge(cpyassets);
 });
 
-// Minify CSS
-gulp.task('minify:css', function() {
-    return gulp.src([
-            paths.src.css + '/material-kit.css'
-        ])
-        .pipe(cleanCss())
-        .pipe(rename(function(path) {
-            // Updates the object in-place
-            path.extname = ".min.css";
-        }))
-        .pipe(gulp.dest(paths.src.css))
+//  [ Copy assets ] end
+
+//  [ build html ] start
+gulp.task("build-html", function () {
+  return gulp
+    .src("src/html/**/*.html")
+    .pipe(
+      fileinclude({
+        prefix: "@@",
+        basepath: "@file",
+        indent: true,
+      })
+    )
+    .pipe(gulp.dest("dist"));
+});
+//  [ build html ] end
+
+//  [ build js ] start
+gulp.task("build-js", function () {
+  var layoutjs = gulp
+    .src("src/assets/js/*.js")
+    .pipe(gulp.dest("dist/assets/js"));
+
+  var pagesjs = gulp
+    .src("src/assets/js/pages/*.js")
+    .pipe(gulp.dest("dist/assets/js/pages"));
+
+  return merge(layoutjs, pagesjs);
+});
+//  [ build js ] end
+
+//  [ scss compiler ] start
+gulp.task("mincss", function () {
+  // main style css
+  return gulp
+    .src("src/assets/scss/*.scss")
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(cssbeautify())
+    .pipe(gulp.dest("dist/assets/css"))
+    .pipe(cssmin())
+    .pipe(gulp.dest("dist/assets/css"));
+});
+//  [ scss compiler ] end
+
+//  [ uglify js ] start
+gulp.task("uglify", function () {
+  var layoutjs = gulp
+    .src("src/assets/js/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("dist/assets/js"));
+
+  var pagesjs = gulp
+    .src("src/assets/js/pages/*.js")
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(gulp.dest("dist/assets/js/pages"));
+
+  return merge(layoutjs, pagesjs);
+});
+//  [ uglify js ] end
+
+//  [ minify html ] start
+gulp.task("htmlmin", function () {
+  return gulp
+    .src("src/html/**/*.html")
+    .pipe(
+      fileinclude({
+        context: layout,
+        prefix: "@@",
+        basepath: "@file",
+        indent: true,
+      })
+    )
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+      })
+    )
+    .pipe(gulp.dest("dist"));
+});
+//  [ minify html ] end
+
+//  [ image optimizer ] start
+gulp.task("imgmin", function () {
+  return gulp
+    .src("src/assets/img/**/*.{jpg,png}")
+    .pipe(smushit())
+    .pipe(gulp.dest("dist/assets/img"));
+});
+//  [ image optimizer ] end
+
+//  [ browser reload ] start
+gulp.task("browserSync", function () {
+  browsersync.init({
+    server: "dist/",
+  });
+});
+//  [ browser reload ] end
+
+gulp.task("cleandist", function (callback) {
+  del.sync(["dist/*/"]);
+  callback();
 });
 
-// Default Task: Compile SCSS and minify the result
-gulp.task('default', gulp.series('scss', 'minify:css'));
+//  [ watch ] start
+gulp.task("watch", function () {
+  gulp
+    .watch("src/assets/scss/**/*.scss", gulp.series("sass"))
+    .on("change", browsersync.reload);
+  gulp
+    .watch("src/assets/js/**/*.js", gulp.series("build-js"))
+    .on("change", browsersync.reload);
+  gulp
+    .watch("src/html/**/*.html", gulp.series("build-html"))
+    .on("change", browsersync.reload);
+  gulp
+    .watch("src/doc/**/*.html", gulp.series("build"))
+    .on("change", browsersync.reload);
+});
+//  [ watch ] start
+const compile = gulp.parallel("browserSync", "watch");
+
+
+
+//  [ watch minify ] start
+gulp.task("watch-minify", function () {
+  gulp.watch("src/assets/scss/**/*.scss", gulp.series("mincss"));
+  gulp.watch("src/assets/js/**/*.js", gulp.series("uglify"));
+  gulp.watch("src/html/**/*.html", gulp.series("htmlmin"));
+  gulp.watch("src/doc/**/*.html", gulp.series("build"));
+});
+//  [ watch minify ] start
+
+// build in production mode
+gulp.task(
+  "build-prod",
+  gulp.series("sass", "build-js", "build-html", "imgmin")
+);
+
+
+//  [ Default task ] start
+gulp.task(
+  "default",
+  gulp.series("sass")
+);
+//  [ Default task ] end
